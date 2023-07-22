d623cc07ed1b77f365405f1c994703f212364b91
KbVrunda <114193017+KbVrunda@users.noreply.github.com>
2023-07-22 05:07:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import tensorflow as tf
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Load and preprocess your dataset
+def load_dataset():
+    # Load your data into features (X) and labels (y)
+    X = ...
+    y = ...
+    return X, y
+
+# Define the TensorFlow model
+def create_model(input_shape):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=input_shape),
+        # Add your desired layers here
+        tf.keras.layers.Dense(units=128, activation='relu'),
+        tf.keras.layers.Dense(units=64, activation='relu'),
+        tf.keras.layers.Dense(units=1, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+# Train the model
+def train_model(X_train, y_train, epochs=10, batch_size=32):
+    model = create_model(X_train.shape[1:])
+    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+    return model
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+if __name__ == "__main__":
+
+    X_train, y_train = load_dataset()
+
+    # Train the model
+    trained_model = train_model(X_train, y_train)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    trained_model.save("parkai_model.h5")
